985395659f45f2c6851e50dc0272929888b6aac9
prokopec.simon <ic.prokopec@gmail.com>
2022-06-22 11:27:14 +0200

    added admin access

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,12 +2,23 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = process.env
+        .NEXTAUTH_ALLOWED_EMAILS!.split(",")
+        .includes(user.email!);
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
